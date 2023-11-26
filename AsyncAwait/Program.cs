9c6360f0e9321ce736a9fd6ce67667e11d3901d0
Bhavana-cloud <54134373+Bhavana-cloud@users.noreply.github.com>
2023-11-26 12:57:33 +0530

    Add files via upload

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*int count = 0;
+count = await Method3();
+Method4(count);
+Console.WriteLine($"**** value of count = { count}");
+
+void UseTraditionalThreading()
+{
+    Thread t1 = new Thread(new ThreadStart(Method1));
+    Thread t2 = new Thread(new ThreadStart(Method2));
+
+    t1.Start();
+    t2.Start();
+    Thread.Sleep(1000);
+}
+
+
+
+void Method1()
+{
+    for(int i = 0; i < 1000; i++)
+    {
+        Console.WriteLine($"METHOD 1: i={i}");
+    }
+}
+
+void Method2()
+{
+    for(int i=0;i<100;i++)
+    {
+        Console.WriteLine($"METHOD 2: i={i}");
+    }
+}
+
+async Task<int> Method3()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        Console.WriteLine($"METHOD 3: i={i}");
+    }
+    return i;
+}
+
+async void Method4()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        Console.WriteLine($"METHOD 4: i={i}");
+    }
+}*/
+
+await SimpleTask();
+File.WriteAllText(@"SomeFile.txt", "blah blah blah");
+string contents = await ReadFile();
+Console.WriteLine(contents);
+
+async Task SimpleTask()
+{
+    Console.WriteLine("starting of the task");
+    await Task.Delay(1000); //force a delay
+    Console.WriteLine("task Complete");
+}
+
+async Task<string> ReadFile()
+{
+    using (StreamReader r = new StreamReader(@"SomeFile.txt"))
+    {
+        return await r.ReadToEndAsync();
+    }
+}
---
+++ ConsolecCient/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using RestSharp;
+
+Console.WriteLine("Hello, World!");
+
+
+RestClient client = new RestClient();   
+RestRequest req1 = new RestRequest("https://localhost:7082/api/Name/authenticate", Method.Post);
+req1.AddJsonBody(new
+{
+username = "test1",
+password = "password1"
+});
+
+var response1 = client.Execute(req1);
+var token = JsonConvert.DeserializeObject<string>(response1.Content);
+Console.WriteLine(token);
+//REquest 2 - call Authorized PI BY Attaching the token
+RestRequest req2 = new RestRequest("https://localhost:7082/api/Name", Method.Get);
+client.AddDefaultHeader("Authorization", $"Bearer {token}");
+
+var response2 = client.Execute(req2).Content;
+//var content = response2.Content; 
+Console.WriteLine(response2);
+Console.ReadLine();//type enter to exit application
+
